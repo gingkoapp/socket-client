3199a2857265c99c8362a5495861a864d8753c47
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-06-07 14:10:47 +0600

    Implemented main idea of Backbone.Socket

--- index.js
+++ index.js
@@ -4,14 +4,54 @@
 ;(function(Backbone, _) {
   'use strict';
 
-  var Socket = Backbone.Socket = function(options) {
-    this.cid = _.uniqueId('socket');
-    this.initialize.apply(this, options);
+  var Socket = Backbone.Socket = function(socket) {
+    this.cid    = _.uniqueId('socket');
+    this.socket = socket;
+    this.initialize.apply(this, arguments);
   };
 
   _.extend(Socket.prototype, Backbone.Events, {
-    // convention to define constructor
     initialize: function() {},
+
+    add: function(collection) {
+      collection.on('add', this.emitEvent('add', collection), this);
+      collection.on('change', this.emitEvent('change', collection), this);
+      collection.on('remove', this.emitEvent('remove', collection), this);
+    },
+
+    emitEvent: function(event, collection) {
+      var name = event + '-' + collection.socket;
+
+      this.socket.on(name, function(data) {
+        this['on' + event].call(this, collection, data);
+        this.trigger(name, data);
+      }.bind(this));
+
+      return function(model, collection, options) {
+        if (!options) options = collection; // for change event
+        if (options.socket) return;
+
+        var json = model.toJSON();
+        json.t   = Date.now();
+        this.socket.emit(name, json);
+      };
+    },
+
+    onAdd: function(collection, data) {
+      collection.add(data.json, { socket: true });
+    },
+
+    onChange: function(collection, data) {
+      var model = collection.get(data.id);
+      if (!model) return;
+      model.set(data.json, { socket: true });
+    },
+
+    onRemove: function(collection, data) {
+      var model = collection.get(data.id);
+      if (!model) return;
+      model.remove(data.json, { socket: true });
+    }
   });
 
   Socket.extend = Backbone.Model.extend;
